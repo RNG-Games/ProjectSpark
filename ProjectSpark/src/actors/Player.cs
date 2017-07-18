672c737bba02426427540ffde830e96fa5b79359
kiessler <jonas.kiessler@st.ovgu.de>
2017-07-18 23:36:32 +0200

    Added Player and Gravity

---
+++ ProjectSpark/src/actors/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _ProjectSpark.util;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace _ProjectSpark.actors
+{
+    class Player : IActable
+    {
+        Vector2f position;
+        Sprite texture;
+
+        Vector2f gravity = new Vector2f(0, 500);
+        Vector2f velocity = new Vector2f(0, 500);
+
+        public Player()
+        {
+            position = new Vector2f(1000, 0);
+            texture = new Sprite(Resources.GetTexture("player.png")) { Position = position };
+        }
+
+        public void Draw(RenderWindow _window)
+        {
+            _window.Draw(texture);
+        }
+
+        public bool IsExpired()
+        {
+            return false;
+        }
+
+        public float StartTime()
+        {
+            return 0f;
+        }
+
+        public void Update(float _deltaTime)
+        {
+            position += _deltaTime * velocity;
+            velocity += _deltaTime * gravity;
+            texture.Position = position;
+        }
+    }
+}
--- ProjectSpark/src/gamestates/MainState.cs
+++ ProjectSpark/src/gamestates/MainState.cs
@@ -2,6 +2,8 @@
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
+using _ProjectSpark.actors;
+
 
 namespace _ProjectSpark.gamestates
 {
@@ -11,16 +13,19 @@ namespace _ProjectSpark.gamestates
 	    private Vertex[] line = {new Vertex(new Vector2f(50, 50)), new Vertex(new Vector2f(100,100)) };
 	    private float _time = 0;
         Text text = new Text(){ Font = new Font(Resources.GetFont("trebuc.ttf"))};
+        Player player = new Player();
+
 	    public override void Draw(RenderWindow _window)
 		{
-		    text.DisplayedString = "Nothing to see here (for now)";
+		    text.DisplayedString = "FALLING CIRCLE!!!!";
             text.Position = new Vector2f(200, 200);
-
-		    testSprite.Scale = new Vector2f(64, 64);
+            _window.Clear(new Color(0, 0, 0));
+            testSprite.Scale = new Vector2f(64, 64);
 			_window.Draw(testSprite);
             //_window.SetView(_view);
             _window.Draw(line,0,2, PrimitiveType.Lines);
             _window.Draw(text);
+            player.Draw(_window);
 		}
 
 		public override void Update(float _deltaTime)
@@ -28,7 +33,7 @@ namespace _ProjectSpark.gamestates
 		    _time += _deltaTime;
             _view.Zoom(1.00005f);
             if(Keyboard.IsKeyPressed(Keyboard.Key.V)) testSprite.Position += new Vector2f(2f, 2f);
-
+            player.Update(_deltaTime);
 		}
 	    private View _view = new View(new Vector2f(0,0), new Vector2f(200, 200));
 	}

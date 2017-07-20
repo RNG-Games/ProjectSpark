f693a2440b36911f155a1538a8e224bf4e827f07
kiessler <jonas.kiessler@st.ovgu.de>
2017-07-20 23:58:03 +0200

    Implemented Actor-List

--- ProjectSpark/src/gamestates/MainState.cs
+++ ProjectSpark/src/gamestates/MainState.cs
@@ -5,55 +5,56 @@ using SFML.Window;
 using _ProjectSpark.actors;
 using _ProjectSpark.actors.blocks;
 using _ProjectSpark.actors.enemies;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _ProjectSpark.gamestates
 {
 	class MainState : GameState
 	{
-		//Sprite testSprite = new Sprite(Resources.GetTexture("pixel.png"));
-	    //private Vertex[] line = {new Vertex(new Vector2f(50, 50)), new Vertex(new Vector2f(100,100)) };
 	    private float _time = 0;
         Text text = new Text(){ Font = new Font(Resources.GetFont("trebuc.ttf"))};
-        Player player = Player.getPlayer();
-        Block bl1 = new Regular(15, 15);
-        LargeBlock bl2 = new LargeBlock("regular", 4, 9, 5, 10);
-        LargeBlock bl3 = new LargeBlock("spike", 4, 9, 4, 4);
-        Block bl4 = new Spike(15, 14);
-        Line test = new Line(new Vector2f(9, 30), 20);
-        Block bl5 = new Trampoline(25, 14);
-        Enemy e1 = new Stationary(26, 14);
 
+        Player player = Player.getPlayer();    
+        List<IActable> actors = new List<IActable>();
 
-	    public override void Draw(RenderWindow _window)
-		{
+        //test area       
+        public MainState()
+        {
+            // blocks
+            actors.Add(new Regular(15, 15));
+            actors.Add(new LargeBlock("regular", 4, 9, 5, 10));
+            actors.Add(new LargeBlock("spike", 4, 9, 4, 4));
+            actors.Add(new Spike(15, 14));
+            actors.Add(new Trampoline(25, 14));
+
+            // enemies
+            actors.Add(new Stationary(26, 14));
+
+            // lines
+            actors.Add(new Line(new Vector2f(9, 30), 20));
+        }
+
+        public override void Draw(RenderWindow _window) {
 		    text.DisplayedString = "FALLING CIRCLE!!!!";
             text.Position = new Vector2f(200, 200);
-            _window.Clear(new Color(0, 0, 0));
-            //testSprite.Scale = new Vector2f(64, 64);
-			//_window.Draw(testSprite);
-            //_window.Draw(line,0,2, PrimitiveType.Lines);
+            _window.Clear(new Color(0, 0, 0));			
+
+
             _window.Draw(text);
-            bl1.Draw(_window);
-            bl2.Draw(_window);
-            bl3.Draw(_window);
-            bl4.Draw(_window);
-            bl5.Draw(_window);
-            e1.Draw(_window);
+
+            foreach (var actor in actors)
+                actor.Draw(_window);
             player.Draw(_window);
-            test.Draw(_window);
         }
 
 		public override void Update(float _deltaTime)
 		{
 		    _time += _deltaTime;
-            //if(Keyboard.IsKeyPressed(Keyboard.Key.V)) testSprite.Position += new Vector2f(2f, 2f);
-            bl1.Update(_deltaTime);
-            bl2.Update(_deltaTime);
-            bl3.Update(_deltaTime);
-            bl4.Update(_deltaTime);
-            bl5.Update(_deltaTime);
-            e1.Update(_deltaTime);
-            test.Update(_deltaTime);
+            actors = actors.Where(a => !a.IsExpired()).ToList();
+            foreach (var actor in actors)
+                actor.Update(_deltaTime);
+
             player.Update(_deltaTime);
         }
 

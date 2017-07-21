4ea487b10770170de1db0756d994a5e3611347d3
kiessler <jonas.kiessler@st.ovgu.de>
2017-07-21 23:32:08 +0200

    Added a block that does nothing

---
+++ ProjectSpark/src/actors/blocks/Decoration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _ProjectSpark.util;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace _ProjectSpark.actors.blocks
+{
+    class Decoration : Block
+    {
+        public Decoration(int x, int y, string texture) : base(x, y)
+        {
+            this.texture = new Sprite(Resources.GetTexture(texture)) { Position = position };
+        }
+    }
+}

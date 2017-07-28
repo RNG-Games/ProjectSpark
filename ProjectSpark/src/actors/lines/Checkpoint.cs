21f7b57941cec5242b53bd835d40409a0c80d0cc
kiessler <jonas.kiessler@st.ovgu.de>
2017-07-28 16:53:17 +0200

    Ported Lines (functionality is WIP)

---
+++ ProjectSpark/src/actors/lines/Checkpoint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectSpark.assets;
+using ProjectSpark.util;
+using TwistedLogik.Ultraviolet;
+using TwistedLogik.Ultraviolet.Content;
+using TwistedLogik.Ultraviolet.Graphics.Graphics2D;
+using TwistedLogik.Ultraviolet.Input;
+
+namespace ProjectSpark.actors.lines
+{
+    class Checkpoint : Line
+    {
+        public Action setCheckpoint;
+        bool start = false;
+        public Checkpoint(bool start, Vector2f borders, float y) : base(borders, y)
+        {
+            this.start = start;
+            if (start)
+            {
+                Player.getPlayer().SetCurrLine(position - scale / 2, this);
+                Player.getPlayer().position = new Vector2f(this.borders.X + Math.Abs(this.borders.Y - this.borders.X)/2, y);
+            }
+        }
+
+        public override void Update(UltravioletTime time)
+        {
+
+            base.Update(time);
+            if (onLine)
+            {
+                if (!start) setCheckpoint?.Invoke();
+                //if (Keyboard.IsKeyPressed(Keyboard.Key.Space)) resetLine();
+                Vector2f plPos = Player.getPlayer().position;
+                if (Player.getPlayer().leftBorder < borders.X - scale / 2) Player.getPlayer().leftBorder = (int) borders.X - scale / 2;
+                if (Player.getPlayer().rightBorder > borders.Y + scale / 2) Player.getPlayer().rightBorder = (int) borders.Y + scale / 2;
+            }
+        }
+
+        
+    }
+}
---
+++ ProjectSpark/src/actors/lines/Transition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectSpark.assets;
+using ProjectSpark.util;
+using TwistedLogik.Ultraviolet;
+using TwistedLogik.Ultraviolet.Content;
+using TwistedLogik.Ultraviolet.Graphics.Graphics2D;
+using TwistedLogik.Ultraviolet.Input;
+
+namespace ProjectSpark.actors.lines
+{
+    class Transition : Line
+    {        
+
+        public Transition(Vector2f borders, float y) : base(borders, y) {}
+
+        public override void Update(UltravioletTime time)
+        {
+            base.Update(time);
+            if (onLine)
+            {
+
+                if (!enabled){
+                    //Program.MoveCameraDown(position - scale, 3, resetLine);
+                }
+                enabled = true;
+                Vector2f plPos = Player.getPlayer().position;
+                if (Player.getPlayer().leftBorder < borders.X - scale / 2) Player.getPlayer().leftBorder = (int)borders.X - scale / 2;
+                if (Player.getPlayer().rightBorder > borders.Y + scale / 2) Player.getPlayer().rightBorder = (int)borders.Y + scale / 2;
+            }
+        }
+    }
+}
